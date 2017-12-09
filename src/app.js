ac7c8ed0a99f9e8dc9b4cde94661f1dd6bb025b2
André Ramos <andrehhtm@gmail.com>
2017-12-09 00:34:18 -0200

    Commit Inicial

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+	render: function () {
+		// return <h1>Títuloe</h1>
+		return <div>Aplicaçã00</div>
+	}
+})
+
+export default App
