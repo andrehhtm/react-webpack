eb3021a7de5589ed7ad1b17d1a3146cf0275f5bd
Andre Ramos <andrehhtm@gmail.com>
2019-02-25 21:25:23 -0300

    Security updates npm

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import React from 'react'
 const App = React.createClass({
 	render: function () {
 		// return <h1>Títuloe</h1>
-		return <div>Aplicaçã00</div>
+		return <div>Aplicação</div>
 	}
 })
 
